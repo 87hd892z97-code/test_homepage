c020a7df201edfc81c41b435783a83bf411c3675
87hd892z97-code <daichan.wpptprqw@gmail.com>
2025-10-24 20:13:09 +0900

    vercel適用

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,  // 開発環境での画像表示を確実にする
+    unoptimized: true,  // Vercelでの画像表示を確実にする
     disableStaticImages: false,  // 静的画像の最適化を有効に
   },
+  experimental: {
+    serverComponentsExternalPackages: ['bcrypt']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('bcrypt');
+    }
+    return config;
+  }
 }
 
 module.exports = nextConfig
